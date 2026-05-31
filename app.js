a0ace40d75f37663db8a852cf871ebf1bd1cd050
mzzdup-cmd <mzzdup@gmail.com>
2026-05-31 15:20:38 +0300

    app.js

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-const CORRECT_PASSWORD = "1234";
+console.log("JS ЗАГРУЗИЛСЯ");
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  const loginBtn = document.getElementById("loginBtn");
+  const btn = document.getElementById("loginBtn");
   const error = document.getElementById("error");
-  const loginScreen = document.getElementById("loginScreen");
-  const app = document.getElementById("app");
 
-  loginBtn.addEventListener("click", () => {
-
-    const pass = document.getElementById("password").value;
-
-    if (pass === CORRECT_PASSWORD) {
-      loginScreen.classList.add("hidden");
-      app.classList.remove("hidden");
-    } else {
-      error.innerText = "Неверный пароль";
-    }
-
+  btn.addEventListener("click", () => {
+    console.log("КНОПКА НАЖАТА");
+    error.innerText = "JS работает";
   });
 
 });
