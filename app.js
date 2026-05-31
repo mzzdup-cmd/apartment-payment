6051738c9212188e4419ff9747d108a8b5881b65
mzzdup-cmd <mzzdup@gmail.com>
2026-05-31 15:22:00 +0300

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-console.log("JS ЗАГРУЗИЛСЯ");
 
-document.addEventListener("DOMContentLoaded", () => {
-
-  const btn = document.getElementById("loginBtn");
-  const error = document.getElementById("error");
-
-  btn.addEventListener("click", () => {
-    console.log("КНОПКА НАЖАТА");
-    error.innerText = "JS работает";
-  });
-
-});
