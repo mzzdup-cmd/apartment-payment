4e6c0542838994a5bd932a6596a2f6f10bd540f7
mzzdup-cmd <mzzdup@gmail.com>
2026-05-31 15:23:21 +0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+  const btn = document.getElementById("loginBtn");
+  const input = document.getElementById("password");
+  const error = document.getElementById("error");
+  const login = document.getElementById("loginScreen");
+  const app = document.getElementById("app");
+
+  const CORRECT_PASSWORD = "1234";
+
+  btn.addEventListener("click", () => {
+
+    const value = input.value;
+
+    if (value === CORRECT_PASSWORD) {
+
+      login.classList.add("hidden");
+      app.classList.remove("hidden");
+
+      console.log("LOGIN OK");
+
+    } else {
+
+      error.innerText = "Неверный пароль";
+
+    }
+
+  });
+
+});
